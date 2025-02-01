6fc8cf3c2099f73cfd8bb67988326c8f5ad50c09
Konstantin Vikhlyantsev <vihlancevk@gmail.com>
2025-02-01 11:52:32 +0300

    [identidock] Create main web-page

--- identidock/app/identidock.py
+++ identidock/app/identidock.py
@@ -1,11 +1,24 @@
 from flask import Flask
 
 app = Flask(__name__)
+default_name = 'Kosty'
 
 
 @app.route('/')
-def hello_world():
-	return 'Hello Docker!\n'
+def get_identicon():
+	name = default_name
+
+	header = '<html><head><title>Identidock</title></head><body>'
+	body = '''<form method="POST">
+			  Hello <input type="text" name="name" value="{}">
+			  <input type="submit" value="submit">
+			  </form>
+			  <p>You look like a:
+			  <img src="/monster/monster.png"/>
+			  '''.format(name)
+	footer = '</body></html>'
+
+	return header + body + footer
 
 
 if __name__ == '__main__':
