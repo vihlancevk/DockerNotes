a3b9f6ed05d52b39f8ea6ef02a0ceba4a2652455
Konstantin Vikhlyantsev <vihlancevk@gmail.com>
2025-02-01 15:56:41 +0300

    [identidock] Use dnmonster to genetate identification pictogram

--- identidock/app/identidock.py
+++ identidock/app/identidock.py
@@ -1,24 +1,38 @@
-from flask import Flask
+import hashlib
+import requests
+
+from flask import Flask, Response, request
 
 app = Flask(__name__)
+salt = "UNIQUE_SALT"
 default_name = 'Kosty'
 
 
-@app.route('/')
-def get_identicon():
-	name = default_name
+@app.route('/', methods=['GET', 'POST'])
+def mainpage():
+	name = request.form['name'] if request.method == 'POST' else default_name
+	salted_name = salt + name
+	name_hash = hashlib.sha256(salted_name.encode()).hexdigest()
 
 	header = '<html><head><title>Identidock</title></head><body>'
 	body = '''<form method="POST">
-			  Hello <input type="text" name="name" value="{}">
+			  Hello <input type="text" name="name" value="{0}">
 			  <input type="submit" value="submit">
 			  </form>
 			  <p>You look like a:
-			  <img src="/monster/monster.png"/>
-			  '''.format(name)
+			  <img src="/monster/{1}"/>
+			  '''.format(name, name_hash)
 	footer = '</body></html>'
 
 	return header + body + footer
+
+
+@app.route('/monster/<name>')
+def get_identicon(name):
+	r = requests.get('http://dnmonster:8080/monster/' + name + '?size=80')
+	image = r.content
+
+	return Response(image, mimetype='image/png')
 
 
 if __name__ == '__main__':
